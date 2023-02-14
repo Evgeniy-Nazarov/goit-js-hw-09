21ddd19816c67824477c2804d044f22eebbdddbe
Evgeniy-Nazarov <91555886+Evgeniy-Nazarov@users.noreply.github.com>
2023-02-14 19:57:35 +0000

    Deploying to gh-pages from @ Evgeniy-Nazarov/goit-js-hw-09@2c0ec67769c9eec6a186c4afafe632d6e4e3cf15 🚀

---
+++ 01-color-switcher.cc08037e.js
@@ -0,0 +1,2 @@
+!function(){function t(){return"#".concat(Math.floor(16777215*Math.random()).toString(16))}var e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]"),r=document.querySelector("body");e.addEventListener("click",(function(){timerId=setInterval((function(){r.style.backgroundColor=t(),e.disabled=!0}),1e3)})),n.addEventListener("click",(function(){clearInterval(timerId),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.cc08037e.js.map
